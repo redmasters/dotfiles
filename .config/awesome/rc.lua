4481712758b9a0898732edd2d0dfe0aec404ebbc
Erasmo <redmaster_undersky@hotmail.com>
2019-12-04 12:12:33 -0300

    Update rc.lua
    Rofi funcionando, falta stylizar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -359,8 +359,9 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
+    --awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    --          {description = "run prompt", group = "launcher"}),
+    awful.key({ modkey }, "r", function () awful.util.spawn(rofi_script) end),
 
     awful.key({ modkey }, "x",
               function ()
